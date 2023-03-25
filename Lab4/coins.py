b20f3b0aeb668b1d60232126dade36cd1a7a67be
Andrzej Rafalski <anraf1001@gmail.com>
2023-03-25 15:05:10 +0100

    Sum up coins value

--- Lab4/coins.py
+++ Lab4/coins.py
@@ -12,12 +12,17 @@ circles = cv2.HoughCircles(
     edges, cv2.HOUGH_GRADIENT, 1, 50, param1=100, param2=20, minRadius=50, maxRadius=100
 )
 circles = np.uint16(np.around(circles))
+
+coins_sum = 0.0
 for i in circles[0, :]:
     if i[2] > 70:
         cv2.circle(image, (i[0], i[1]), i[2], (0, 0, 255), 4)
+        coins_sum += 1.0
     else:
         cv2.circle(image, (i[0], i[1]), i[2], (0, 255, 0), 4)
+        coins_sum += 0.1
 
+print(f"Sum: {coins_sum:.2f} PLN")
 cv2.imshow("image", cv2.resize(image, dsize=None, fx=0.8, fy=0.8))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
