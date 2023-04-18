b29235ce68d099239110e856358507a3168dc6f7
Andrzej Rafalski <anraf1001@gmail.com>
2023-04-18 08:44:57 +0200

    Descriptors

--- Lab8/detect.py
+++ Lab8/detect.py
@@ -41,6 +41,25 @@ cv2.drawKeypoints(
     image, keypoints_sift, image_sift, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
 )
 
+brief = cv2.xfeatures2d.BriefDescriptorExtractor_create()
+tick = time()
+keypoints_fast, desc_brief = brief.compute(image, keypoints_fast)
+print(f"BRIEF time: {time() - tick:.4f} s")
+print(desc_brief)
+print()
+
+tick = time()
+keypoints_orb, desc_orb = orb.compute(image, keypoints_orb)
+print(f"ORB time: {time() - tick:.4f} s")
+print(desc_orb)
+print()
+
+tick = time()
+keypoints_sift, desc_sift = sift.compute(image, keypoints_sift)
+print(f"SIFT time: {time() - tick:.4f} s")
+print(desc_sift)
+print()
+
 cv2.imshow("fast", image_fast)
 cv2.imshow("orb", image_orb)
 cv2.imshow("sift", image_sift)
