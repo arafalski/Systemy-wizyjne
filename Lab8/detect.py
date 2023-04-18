86c4cdd7fd4fac8ad4222279be46ff991ef5e221
Andrzej Rafalski <anraf1001@gmail.com>
2023-04-18 08:47:13 +0200

    Refactor

--- Lab8/detect.py
+++ Lab8/detect.py
@@ -15,9 +15,7 @@ print(f"FAST time: {time() - tick:.4f} s")
 print()
 
 image_fast = np.zeros_like(image)
-cv2.drawKeypoints(
-    image, keypoints_fast, image_fast, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
-)
+cv2.drawKeypoints(image, keypoints_fast, image_fast)
 
 tick = time()
 keypoints_orb = orb.detect(image)
@@ -26,9 +24,7 @@ print(f"OBR time: {time() - tick:.4f} s")
 print()
 
 image_orb = np.zeros_like(image)
-cv2.drawKeypoints(
-    image, keypoints_orb, image_orb, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
-)
+cv2.drawKeypoints(image, keypoints_orb, image_orb)
 
 tick = time()
 keypoints_sift = orb.detect(image)
@@ -37,27 +33,25 @@ print(f"SIFT time: {time() - tick:.4f} s")
 print()
 
 image_sift = np.zeros_like(image)
-cv2.drawKeypoints(
-    image, keypoints_sift, image_sift, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
-)
+cv2.drawKeypoints(image, keypoints_sift, image_sift)
 
 brief = cv2.xfeatures2d.BriefDescriptorExtractor_create()
 tick = time()
 keypoints_fast, desc_brief = brief.compute(image, keypoints_fast)
 print(f"BRIEF time: {time() - tick:.4f} s")
-print(desc_brief)
+print(desc_brief.shape)
 print()
 
 tick = time()
 keypoints_orb, desc_orb = orb.compute(image, keypoints_orb)
 print(f"ORB time: {time() - tick:.4f} s")
-print(desc_orb)
+print(desc_orb.shape)
 print()
 
 tick = time()
 keypoints_sift, desc_sift = sift.compute(image, keypoints_sift)
 print(f"SIFT time: {time() - tick:.4f} s")
-print(desc_sift)
+print(desc_sift.shape)
 print()
 
 cv2.imshow("fast", image_fast)
