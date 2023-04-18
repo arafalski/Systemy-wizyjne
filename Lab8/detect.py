f14da6e364eeb96687bb666110af952bc0b1bf5b
Andrzej Rafalski <anraf1001@gmail.com>
2023-04-18 08:26:49 +0200

    Refactor

--- Lab8/detect.py
+++ Lab8/detect.py
@@ -3,14 +3,14 @@ import numpy as np
 
 image = cv2.imread("images/forward-1.bmp")
 
-detector = cv2.FastFeatureDetector_create()
-descriptor = cv2.ORB_create()
+fast = cv2.FastFeatureDetector_create()
+orb = cv2.ORB_create()
 
-keypoints_fast = detector.detect(image)
+keypoints_fast = fast.detect(image)
 image_fast = np.zeros_like(image)
 cv2.drawKeypoints(image, keypoints_fast, image_fast, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-keypoints_orb = descriptor.detect(image)
+keypoints_orb = orb.detect(image)
 image_orb = np.zeros_like(image)
 cv2.drawKeypoints(image, keypoints_orb, image_orb, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
