cb79111b81b11cd2a1077021e80331e94b86a2f3
Andrzej Rafalski <anraf1001@gmail.com>
2023-04-18 08:25:54 +0200

    ORB detector

--- Lab8/detect.py
+++ Lab8/detect.py
@@ -1,13 +1,21 @@
 import cv2
+import numpy as np
 
 image = cv2.imread("images/forward-1.bmp")
 
 detector = cv2.FastFeatureDetector_create()
+descriptor = cv2.ORB_create()
 
-keypoints = detector.detect(image)
-cv2.drawKeypoints(image, keypoints, image, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+keypoints_fast = detector.detect(image)
+image_fast = np.zeros_like(image)
+cv2.drawKeypoints(image, keypoints_fast, image_fast, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-cv2.imshow("image", image)
+keypoints_orb = descriptor.detect(image)
+image_orb = np.zeros_like(image)
+cv2.drawKeypoints(image, keypoints_orb, image_orb, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+cv2.imshow("fast", image_fast)
+cv2.imshow("orb", image_orb)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
