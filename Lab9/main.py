d209d5982b42aa272e9b34b0d48d9bbdcd08cdb5
Andrzej Rafalski <anraf1001@gmail.com>
2023-05-16 08:49:04 +0200

    Drawdetected rea and inliners

--- Lab9/main.py
+++ Lab9/main.py
@@ -26,7 +26,7 @@ while True:
 
     if len(selected_points) == 4:
         x, y, w, h = cv2.boundingRect(np.array(selected_points))
-        image_cut = image_to_find[y:y + h, x:x + w]
+        image_cut = image_to_find[y : y + h, x : x + w]
 
         kp_cut, desc_cut = detector.detectAndCompute(image_cut, None)
         kp_image, desc_image = detector.detectAndCompute(image_to_match, None)
@@ -37,6 +37,30 @@ while True:
         matches = sorted(matches, key=lambda x: x.distance)
         matches = matches[:20]
 
+        src_pts = np.float32([kp_cut[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp_image[m.trainIdx].pt for m in matches]).reshape(
+            -1, 1, 2
+        )
+
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC)
+        matches_mask = mask.ravel().tolist()
+
+        h, w = image_cut.shape[:2]
+        pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(
+            -1, 1, 2
+        )
+        dst = cv2.perspectiveTransform(pts, M)
+        image_to_match = cv2.polylines(
+            image_to_match, [np.int32(dst)], True, 255, 3, cv2.LINE_AA
+        )
+
+        draw_params = dict(
+            matchColor=(0, 255, 0),
+            singlePointColor=None,
+            matchesMask=matches_mask,
+            flags=2,
+        )
+
         image_matches = cv2.drawMatches(
             image_cut,
             kp_cut,
@@ -44,7 +68,7 @@ while True:
             kp_image,
             matches,
             None,
-            flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
+            **draw_params
         )
 
         cv2.imshow("matches", image_matches)
